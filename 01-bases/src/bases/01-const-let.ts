e0a1799a3c0a21effa519f3357a3d82756cdf29a
jperezreq <jaimeperezrequejo26@gmail.com>
2025-07-18 19:12:44 +0100

    leson 1 Typescript

---
+++ 01-bases/src/bases/01-const-let.ts
@@ -0,0 +1,19 @@
+
+
+const firstName = 'Tony';
+const lastName = 'Stark';
+
+// console.log({firstName, lastName});
+
+
+if (true) {
+    const firstName = 'Peter';
+}
+
+// console.log(firstName)
+
+// console.log(`${firstName} ${lastName}`)
+
+export const fullname = `${firstName} ${lastName}`
+
+// console.log(fullname)
---
+++ 01-bases/src/main.ts
@@ -0,0 +1,21 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+
+
+import './bases/01-const-let';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`

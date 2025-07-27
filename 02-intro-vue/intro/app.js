d182a05d8620ccf58b7f3f1c0659c569016a2983
jperezreq <jaimeperezrequejo26@gmail.com>
2025-07-27 20:09:42 +0100

    use v:on

--- 02-intro-vue/intro/app.js
+++ 02-intro-vue/intro/app.js
@@ -2,24 +2,28 @@
 const { createApp, ref} = Vue;
 
 const app = createApp({
-    template: `
-    <h1> {{ message }}</h1>
-    <p>{{ author }}</p>
-    `,
+    // template: `
+    // <h1> {{ message }}</h1>
+    // <p>{{ author }}</p>
+    // `,
 
     setup() {
         const message = ref("I`m Batman");
         const author = ref(" Bruce Wayne");
 
-
-        setTimeout(() => {
+        const changeQuote = () => {
             message.value = "I`m Spiderman";
             author.value = "Peter Parker";
-        }, 1000);
+        }
+        // setTimeout(() => {
+        //     message.value = "I`m Spiderman";
+        //     author.value = "Peter Parker";
+        // }, 1000);
 
         return {
             message,
             author,
+            changeQuote
         }
     }
 });

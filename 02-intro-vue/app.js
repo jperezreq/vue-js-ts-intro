1398d9c44ebd056dba53132765cf855976a4c101
jperezreq <jaimeperezrequejo26@gmail.com>
2025-07-27 19:55:47 +0100

    using ref
    Ref for reactive variables

---
+++ 02-intro-vue/app.js
@@ -0,0 +1,28 @@
+
+const { createApp, ref} = Vue;
+
+const app = createApp({
+    template: `
+    <h1> {{ message }}</h1>
+    <p>{{ author }}</p>
+    `,
+
+    setup() {
+        const message = ref("I`m Batman");
+        const author = ref(" Bruce Wayne");
+
+
+        setTimeout(() => {
+            message.value = "I`m Spiderman";
+            author.value = "Peter Parker";
+        }, 1000);
+
+        return {
+            message,
+            author,
+        }
+    }
+});
+
+
+app.mount('#myApp');
